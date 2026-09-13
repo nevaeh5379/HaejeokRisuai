dd0daedd16ff35c98926469102c7786df3ddcb1f
nevaeh5379 <230626330+nevaeh5379@users.noreply.github.com>
2026-09-14 06:40:36 +0900

    refactor(android): consolidate plugin registration into single registerPlugins call

--- android/app/src/main/java/co/aiclient/risu/MainActivity.java
+++ android/app/src/main/java/co/aiclient/risu/MainActivity.java
@@ -1,20 +1,29 @@
 package co.aiclient.risu;
 
 import android.os.Bundle;
+
 import androidx.activity.OnBackPressedCallback;
+
 import com.getcapacitor.BridgeActivity;
 
+import java.util.List;
+
 public class MainActivity extends BridgeActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        registerPlugin(StreamedFetchPlugin.class);
-        registerPlugin(StreamFileWriterPlugin.class);
-        registerPlugin(NativeBackupPlugin.class);
-        registerPlugin(NativeSqlitePlugin.class);
-        registerPlugin(NativeImagePlugin.class);
-        registerPlugin(NativeChatPlugin.class);
-        registerPlugin(NativeAppControlPlugin.class);
-        registerPlugin(NativeUpdaterPlugin.class);
+        registerPlugins(
+                List.of(
+                        StreamedFetchPlugin.class,
+                        StreamFileWriterPlugin.class,
+                        NativeBackupPlugin.class,
+                        NativeSqlitePlugin.class,
+                        NativeImagePlugin.class,
+                        NativeChatPlugin.class,
+                        NativeAppControlPlugin.class,
+                        NativeUpdaterPlugin.class
+                ));
         super.onCreate(savedInstanceState);
         if (getBridge() != null && getBridge().getWebView() != null) {
             getBridge().setWebViewClient(new RisuWebViewClient(getBridge(), getApplicationContext()));
@@ -27,8 +36,8 @@ public class MainActivity extends BridgeActivity {
                     return;
                 }
                 getBridge().getWebView().evaluateJavascript(
-                    "window.dispatchEvent(new Event('risu:android-back'))",
-                    null
+                        "window.dispatchEvent(new Event('risu:android-back'))",
+                        null
                 );
             }
         });
